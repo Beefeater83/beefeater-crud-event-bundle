e191f6e5d207157954bb638c01b114266de9d0d2
sergey dyakonov <beefeater83@gmail.com>
2025-07-29 21:32:30 +0300

    loader tested

---
+++ Tests/Unit/Routing/CrudRouteLoaderTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Beefeater\CrudEventBundle\Tests\Unit\Routing;
+
+use Beefeater\CrudEventBundle\Routing\CrudRouteLoader;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Routing\RouteCollection;
+
+class CrudRouteLoaderTest extends TestCase
+{
+    private string $yamlPath;
+    protected function setUp(): void
+    {
+        $this->yamlPath = __DIR__ . '/crud_routes.yaml';
+    }
+
+    public function testLoadReturnsRouteCollection(): void
+    {
+        $params = $this->createMock(ParameterBagInterface::class);
+        $params->method('resolveValue')->willReturn($this->yamlPath);
+
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $loader = new CrudRouteLoader($params, $logger);
+
+        $routes = $loader->load($this->yamlPath, 'crud_routes');
+
+        $this->assertInstanceOf(RouteCollection::class, $routes);
+
+        $this->assertCount(12, $routes);
+
+        $routeName = 'api_v1_tournaments_C';
+
+        $route = $routes->get($routeName);
+        $this->assertSame('/api/v1/tournaments', $route->getPath());
+
+        $controller = $route->getDefault('_controller');
+        $this->assertStringContainsString('CrudEventController', $controller);
+        $this->assertStringContainsString('create', $controller);
+    }
+}
